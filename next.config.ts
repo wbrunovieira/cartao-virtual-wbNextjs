acfa9caa59af9e6114984e731b31e1c87bdbc18d
Bruno MacM3 <wbrunovieira77@gmail.com>
2025-04-05 15:52:00 -0300

    ädded custom colors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: false,
+  },
 };
 
 export default nextConfig;
